3d365d68ca860c7221d78e7cd1f3515967f67eee
FirryFinch <93075029+FirryFinch@users.noreply.github.com>
2021-12-17 23:26:10 +0300

    Delete urls.py

--- urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from lab4_app import views
-urlpatterns = [
- path('admin/', admin.site.urls),
- path('', views.engines, name='main_url'),
- path('engine/<int:id>', views.engine, name='engine_url'),
-]
