ba8174a837c214a162ae846dbf99f888060142c5
FirryFinch <93075029+FirryFinch@users.noreply.github.com>
2021-12-19 12:30:12 +0300

    Add files via upload

--- Лаб4/lab4/urls.py
+++ Лаб4/lab4/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from lab4_app import views
 urlpatterns = [
  path('admin/', admin.site.urls),
- path('', views.engines, name='main_url'),
- path('engine/<int:id>', views.engine, name='engine_url'),
+ path('', views.btfmovies, name='main_url'),
+ path('btfmovie/<int:id>', views.btfmovie, name='btfmovie_url'),
 ]
